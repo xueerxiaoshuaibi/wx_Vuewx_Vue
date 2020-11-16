82fd2146b957278119c60c46de24996190f4e6be
xueerxiaoshuaibi <1040256957@qq.com>
2020-11-16 15:57:18 +0800

    wx的vue项目首次提交

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app" @click="test">{{ message }} <router-view /></div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      message: 'Hello commander!'
+    }
+  },
+  methods: {
+    test (e) {
+      this.message = '123'
+      console.log(e)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
